cd69f4344afce48e2d64e06d6b788b0cc5065593
zaenalrfn <ZAOfficial24@gmail.com>
2025-06-13 21:57:30 +0700

    main: auth

--- start/routes.ts
+++ start/routes.ts
@@ -10,12 +10,15 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 const RegisterContollersController = () => import('#controllers/Auth/register_contollers_controller')
+const AuthController = () => import('#controllers/Auth/auth_controller')
 router.on('/').renderInertia('home')
 
 router
   .group(() => {
     router.get('register', [RegisterContollersController, 'index']).as('auth.register')
     router.post('register', [RegisterContollersController, 'store']).as('auth.register.store')
+    router.get('login', [AuthController, 'index']).as('auth.login')
+    router.post('login', [AuthController, 'store']).as('auth.login.store')
   })
   .prefix('/auth')
   .use(middleware.guest())
